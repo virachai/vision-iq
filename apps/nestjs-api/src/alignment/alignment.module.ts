0690f62abc1d10d9df4b20e1a91852dbe8d31c7c
Virachai Wongsena <wvirachai@gmail.com>
2026-02-13 18:21:53 +0700

    Fixing nestjs-api Dependency Injection

--- apps/nestjs-api/src/alignment/alignment.module.ts
+++ apps/nestjs-api/src/alignment/alignment.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { DeepSeekModule } from "../deepseek-integration/deepseek.module";
+import { ImageAnalysisModule } from "../image-analysis/image-analysis.module";
 import { PexelsIntegrationModule } from "../pexels-sync/pexels-integration.module";
 import { QueueModule } from "../queue/queue.module";
 import { SemanticMatchingModule } from "../semantic-matching/semantic-matching.module";
@@ -7,13 +8,14 @@ import { AlignmentController } from "./alignment.controller";
 import { AlignmentService } from "./alignment.service";
 
 @Module({
-	imports: [
-		DeepSeekModule,
-		SemanticMatchingModule,
-		PexelsIntegrationModule,
-		QueueModule,
-	],
-	controllers: [AlignmentController],
-	providers: [AlignmentService],
+  imports: [
+    DeepSeekModule,
+    SemanticMatchingModule,
+    PexelsIntegrationModule,
+    QueueModule,
+    ImageAnalysisModule,
+  ],
+  controllers: [AlignmentController],
+  providers: [AlignmentService],
 })
 export class AlignmentModule {}
